ce24a09aa51c706e75c8f60dae06ce02e6093bf5
moroo0308 <moroo0308@gmail.com>
2018-11-20 06:59:30 +0000

    [Add]test2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	
+	before_action :room_user
+	before_action :cart_user
+	before_action :buy_user
+
+	def room_user
+		if Room.where(user_id: current_user.id).exists?
+		else
+			@room = Room.new(user_id: current_user.id)
+			@room.save
+		end
+	end
+	def cart_user
+		if Cart.where(user_id: current_user.id).exists?
+		else
+			@cart = Cart.new(user_id: current_user.id)
+			@cart.save
+		end
+	end
+	def buy_user
+		if Buy.where(user_id: current_user.id).exists?
+		else
+			@buy = Buy.new(user_id: current_user.id)
+			@buy.save
+		end
+	end
 
   protected
   def configure_permitted_parameters
