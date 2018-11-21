44fa291b73fde745c548b1adee1816f59b837f2b
mondem1010 <mondem1010>
2018-11-21 07:45:35 +0000

    test_commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,7 @@ class ProductsController < ApplicationController
 
 
   def index
-    @products = Product.all
-    @product = Product.new
+    @product = Product.all
     @users = User.all
     @user = current_user
   end
@@ -40,12 +39,15 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
   private
   def product_params
 
-    params.require(:product).permit(:artist,:album,:title,:image,:label,:genre,:status,:introduction,:count,:price)
+    params.require(:product).permit(:artist,:album,:title,:image,:label,:genre,:status,:introduction,:count,:price,:new_price)
   end
 
 end
