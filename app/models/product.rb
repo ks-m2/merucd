6ae928ee22e65fa04e7eedddd4809e91628aa742
mondem1010 <mondem1010>
2018-11-22 03:54:27 +0000

    add validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
 	 	rock: 6, ballad: 7, electronuc: 8, classic: 9, metal: 10}
 	 # enum genre: {aaa: 0, abb: 1, ccc: 3}
 	 enum status: {新品: 0, 未使用に近い: 1, 目立った傷汚れなし: 2, 傷汚れあり: 3, 状態が悪い: 4}
+	 
 	 validates :artist, {presence:true}
 	 validates :album, {presence:true}
 	 validates :label, {presence:true}
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, {presence:true}
    has_many :mails
    has_many :products
    has_one :room
