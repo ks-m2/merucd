6cd4563f6ac48d4e4ad63a0e44cf383c0b38e66c
test1-2-3 <mozya131@gmail.com>
2018-11-26 10:58:42 +0000

    product index fix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def search #わかりにくいけどapplicationのサーチとは同名の別アクション。本当は別名にしたいけどすることで支障でるかよくわからなくて怖いからこのままにしてる。
-    @products = Product.page(params[:page]).reverse_order
+    @products = Product.page(params[:page]).per(20).order('updated_at DESC')
     @users = User.all
     @user = current_user
     # ここ以下ransack（検索機能用）
